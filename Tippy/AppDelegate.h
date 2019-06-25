4d326b1f58c348c1d25362bd235efc3372919e8f
Briana Antiri <antiriby@fb.com>
2019-06-25 13:51:29 -0700

    Initial Commit

---
+++ Tippy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tippy
+//
+//  Created by antiriby on 6/25/19.
+//  Copyright © 2019 antiriby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
