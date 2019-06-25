5780a75e240f68e834cfa404333140162f1c9a16
Briana Antiri <antiriby@fb.com>
2019-06-25 15:57:23 -0700

    New changes

--- Tippy/ViewController.m
+++ Tippy/ViewController.m
@@ -10,6 +10,13 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
 @end
 
 @implementation ViewController
@@ -19,5 +26,49 @@
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)onTap:(id)sender {
+    //In other languages: self.view.endEditing(True);
+    [self.view endEditing:(YES)];
+}
 
+- (IBAction)onEdit:(id)sender {
+    
+    //Tip Calculations
+    NSArray *percentages = @[@(0.15), @(0.2), @(0.22)];
+    
+    double tipPercentage = [percentages[self.tipControl.selectedSegmentIndex] doubleValue];
+    double bill = [self.billField.text doubleValue];
+    double tip = tipPercentage * bill;
+    double total = bill + tip;
+    
+    //Display
+    self.tipLabel.text = [NSString stringWithFormat: @"$%.2f",tip];
+    
+    self.totalLabel.text = [NSString stringWithFormat: @"$%.2f",total];
+    
+}
+
+- (IBAction)onEditingBegin:(id)sender {
+    [UIView animateWithDuration:0.2 animations:^{
+
+    self.billField.frame = CGRectMake(
+        self.billField.frame.origin.x,
+        self.billField.frame.origin.y + 30,
+        self.billField.frame.size.width,
+        self.billField.frame.size.height);
+    self.tipLabel.alpha = 1;
+        
+    }];
+}
+- (IBAction)onEditingEnd:(id)sender {
+    
+    CGRect newFrame = self.billField.frame;
+    newFrame.origin.y -= 30;
+    
+    [UIView animateWithDuration:0.2 animations:^{
+        self.billField.frame = newFrame;
+        
+        self.tipLabel.alpha = 0;
+    }];
+}
 @end
